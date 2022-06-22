a7c147f948c40a7223c7c41cd37ed27a16f7a641
SebaGenetic <seba.powa@gmail.com>
2022-06-22 19:46:49 -0300

    - Started server, dblite created

--- MVTSebastianLeyton/urls.py
+++ MVTSebastianLeyton/urls.py
@@ -1,4 +1,4 @@
-"""MVTSebastianLeyton1 URL Configuration
+"""MVTSebastianLeyton URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- MVTSebastianLeyton/wsgi.py
+++ MVTSebastianLeyton/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MVTSebastianLeyton1 project.
+WSGI config for MVTSebastianLeyton project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MVTSebastianLeyton1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MVTSebastianLeyton.settings')
 
 application = get_wsgi_application()
