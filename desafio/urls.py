fdf46e361f0f86442e009a3209027d80936ce216
SebaGenetic <seba.powa@gmail.com>
2022-06-22 21:08:47 -0300

    - Bootstrap index.html and css added

--- desafio/urls.py
+++ desafio/urls.py
@@ -2,7 +2,8 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import view
+from .views import view, template
 urlpatterns = [
     path('', view),
+    path('template', template)
 ]
--- desafio/views.py
+++ desafio/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Prueba
+
 
 # Create your views here.
 
 def view(request):
-    return HttpResponse('hola')
+    return render(request, 'index.html')
+
+
+def template(request):
+
+    prueba1 = Prueba(nombre='Carlos')
+    prueba2 = Prueba(nombre='Ricardo')
+    prueba3 = Prueba(nombre='Stuart')
+
+    return render(request, 'mi_template.html', {'lista_objetos': [prueba1, prueba2, prueba3]})
